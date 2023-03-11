affb2c619d55dd81072b05cdacc0ea83fbf7d040
kamlesh_patel <59503901+kamlesh-rkj@users.noreply.github.com>
2023-03-11 13:21:30 +0530

    Update TheWebServerProblem.py

--- The Web Server Problem/TheWebServerProblem.py
+++ The Web Server Problem/TheWebServerProblem.py
@@ -1,19 +1,23 @@
 import time
-f=open("data.txt","r")
+from functools import lru_cache
+
+
+f=open("./data.txt","r")
 mspeed = 2
 mloadtime = 60
+@lru_cache(maxsize=None)
 def serve_chunk(data_size):
     chunk_size = 2.5 if data_size <= 5 else 4
     num_chunks = data_size / chunk_size
     for i in range(int(num_chunks)):
         time.sleep(chunk_size / mspeed)
-        print(f"Serving  {i+1} of {num_chunks} ({chunk_size} MB)...")
+    
 
 websites = []
 wb=1
 for i in f.readlines():
     nl=i.split()
-    print(nl)
+    # print(nl)
     web={
         
         "name": f"Website {wb}",
@@ -24,22 +28,23 @@ for i in f.readlines():
     wb+=1
     websites.append(web)
 
-
-# Iterate over each website and serve its pages
 for website in websites:
-    print(f"Serving {website['name']}...")
     
-    # Serve the homepage text content
+   
     serve_chunk(website["homepage_text_size"])
     
-    # Serve the homepage images
+
     serve_chunk(website["homepage_image_size"])
-    
-    
+
+    serve_chunk(website["homepage_form_size"])
+
+
     # Check if the website has loaded within the maximum load time
-    total_load_time = (website["homepage_text_size"] + website["homepage_image_size"]) / mspeed
+    total_load_time = (website["homepage_text_size"] + website["homepage_image_size"]+website["homepage_form_size"]) / mspeed
+    print(str(website["homepage_text_size"] / mspeed)+","+ str(website["homepage_image_size"] / mspeed)+","+str(website["homepage_form_size"] / mspeed))
     if total_load_time > mloadtime:
         print(f"{website['name']} took too long to load!")
     else:
-        print(f"{website['name']} loaded successfully within {total_load_time} seconds.")
+        pass
+
 
